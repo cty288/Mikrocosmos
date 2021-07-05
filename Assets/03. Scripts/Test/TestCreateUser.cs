13ae2e444deb515da2ae80461f4b0fd1406a1eb5
cty288 <cty288@163.com>
2021-07-06 02:47:50 +0800

    NHibernate&MySQL数据库底层初步完成(头发没了)
    终于把这玩意做出来了.....太难整了..
    1. 使用VS在外部创建工程, 引入MySQL和NHibernate初步创建数据库的底层操作(NHibernate Helper Class). 然后将外部工程打包成dll导入unity
    2. 已完成的底层功能包括: 添加数据, 删除数据(还有bug), 通过id查找数据, 通过多个column查找多个数据.  实现这些功能无需写sql代码, 直接调用函数即可
    3. 为数据库建立user表, 接下来重构注册和登录功能, 使其与user表挂钩
    4. 剩下的bug明天再弄.....

---
+++ Assets/03. Scripts/Test/TestCreateUser.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MikrocosmosDatabase;
+using UnityEngine;
+
+public class TestCreateUser : MonoBehaviour {
+    public string username;
+    public int id;
+    public string password;
+    public string email;
+    private UserTableManager manager;
+    void Start() {
+        manager = new UserTableManager();
+        NHibernateHelper.filePath = Application.streamingAssetsPath;
+    }
+    public void OnCreateUserButtonClicked() {
+        User user = new User() {Email = email, Username = username, Password = password, LastLoginTime = DateTime.Now};
+        manager.Add(user);
+    }
+
+    public void OnDeleteUserButtonClicked() {
+        User user = new User() {Id = id};
+        manager.Remove(user);
+    }
+
+    public void OnSearchByIdButtonClicked() {
+        User result= manager.GetById(id);
+        if (result!=null) {
+            Debug.Log($"found {result.Username}");
+        }
+       
+    }
+
+    public void OnSearchByFieldNameButtonClicked() {
+        string[] fieldNames = new string[] {"Username", "Password"};
+        object[] values = new object[] {username, password};
+        IList<User> results= manager.SearchByFieldNames(fieldNames, values);
+        Debug.Log($"found id {results[0].Id}");
+    }
+}
